4702317d9d38dddeee7083f42a54ddbc8a369f5c
Michael Olatunji <tundexmike@gmail.com>
2020-11-02 12:46:39 +0100

    application configurations, tailwind, sass, webpack, react-app-rewired

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    "@typescript-eslint/explicit-function-return-type": "off",
+    // 'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    // 'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.tsx', '**/test.ts'] }],
+    '@typescript-eslint/indent': [2, 2],
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,16 @@
 const { override } = require('customize-cra');
 const { addReactRefresh } = require('customize-cra-react-refresh');
 
-module.exports = override(addReactRefresh());
+// module.exports = override(addReactRefresh());
+
+module.exports = function (config, env) {
+  console.log('Overide!');
+  let loader = config.module.rules[1].oneOf;
+  loader.splice(loader.length - 1, 0, {
+    test: /\.scss$/,
+    use: ['style-loader', 'css-loader', 'sass-loader'],
+  });
+  
+  override(addReactRefresh());
+  return config;
+};
